3ae567ecd810b3c5e2d2e814a9d8f9ea508d7771
Mario Segura <mario@kraferd.com>
2016-06-24 12:34:35 -0700

    Initial Commit

---
+++ DeviceChecker/DeviceChecker.h
@@ -0,0 +1,19 @@
+//
+//  DeviceChecker.h
+//  DeviceChecker
+//
+//  Created by KraferdMBP31 on 6/24/16.
+//  Copyright © 2016 Kraferd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceChecker.
+FOUNDATION_EXPORT double DeviceCheckerVersionNumber;
+
+//! Project version string for DeviceChecker.
+FOUNDATION_EXPORT const unsigned char DeviceCheckerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceChecker/PublicHeader.h>
+
+
---
+++ DeviceChecker/DeviceChecker.swift
@@ -0,0 +1,72 @@
+//
+//  DeviceChecker.swift
+//  DeviceChecker
+//
+//  Created by KraferdMBP31 on 6/24/16.
+//  Copyright © 2016 Kraferd. All rights reserved.
+//
+
+import Foundation
+
+class DeviceChecker {
+    let url : NSURL = NSURL(string: "https://www.example.org/")!;
+    
+    let deviceID : String;
+    
+    let appID : String;
+    
+    //let application : UIApplication;
+    
+    let initialBatteryState : UIDeviceBatteryState;
+    
+    var mainTimer : NSTimer;
+    
+    var count : Int = 0;
+    
+    init(url: NSURL, deviceID: String, appID: String) {
+        //self.url = url;
+        self.deviceID = deviceID;
+        
+        print("\(UIDevice.currentDevice().identifierForVendor!.UUIDString)")
+        
+        self.appID = appID;
+        //self.application = application;
+        
+        self.initialBatteryState = UIDevice.currentDevice().batteryState;
+        
+        self.mainTimer = NSTimer();
+        
+        self.mainTimer = NSTimer(timeInterval: 1.0, target: self, selector: #selector(self.mainTask(_:)), userInfo: nil, repeats: true);
+        
+        NSRunLoop.mainRunLoop().addTimer(self.mainTimer, forMode: NSRunLoopCommonModes);
+    }
+    
+    @objc func mainTask(sender: AnyObject?) {
+        let session = NSURLSession.sharedSession();
+        let request = NSMutableURLRequest(URL: self.url);
+        
+        request.HTTPMethod = "POST"
+        request.cachePolicy = .ReloadIgnoringLocalCacheData;
+        
+        let dataToBeSent : [String : String] = ["app-id" : self.appID]
+        
+        do {
+            let jsonData = try NSJSONSerialization.dataWithJSONObject(dataToBeSent, options: .PrettyPrinted);
+            
+            request.HTTPBody = jsonData;
+            
+            let task = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) -> Void in
+                print("Request Completed \(self.count)");
+                print("Request Data: \(data)")
+                print("Request Response: \(response)");
+                print("Request error: \(error)");
+                self.count = self.count + 1;
+            });
+            
+            task.resume();
+        } catch {
+            print("Error \(error)");
+            return;
+        }
+    }
+}
